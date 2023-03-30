755c9b80026a2c431e56a921cde56dc5d9fe7fc5
Aakash Singh <mail@singhaakash.dev>
2023-03-31 02:28:15 +0530

    fix cache headers

--- src/pages/resume.pdf.ts
+++ src/pages/resume.pdf.ts
@@ -2,7 +2,10 @@ import type { APIRoute } from "astro"
 
 export const get: APIRoute = async function get() {
   try {
-    return fetch("https://sainak.github.io/resume/resume.pdf")
+    let response = await fetch("https://sainak.github.io/resume/resume.pdf")
+    response.headers.set("X-Robots-Tag", "noindex, nofollow")
+
+    return response
   } catch (error: unknown) {
     throw new Error(`Something went wrong!: ${error as string}`)
   }
