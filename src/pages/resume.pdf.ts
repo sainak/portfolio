d89d1b06e10bbd0b091d63749f68dd8d88df5eb7
Aakash Singh <mail@singhaakash.dev>
2023-04-02 03:14:59 +0530

    fix resume route

--- src/pages/resume.pdf.ts
+++ src/pages/resume.pdf.ts
@@ -1,17 +1,15 @@
 import type { APIRoute } from "astro"
 
 export const get: APIRoute = async function get() {
-  try {
-    let response = await fetch("https://sainak.github.io/resume/resume.pdf")
-    response.headers.set("X-Robots-Tag", "noindex, nofollow")
-
-    return response
-  } catch (error: unknown) {
-    console.error(error)
-
-    return {
-      status: 500,
-      body: "Internal Server Error",
+  return fetch("https://sainak.github.io/resume/resume.pdf").then(
+    (res) => {
+      if (res.status === 200) {
+        return res
+      }
+      return new Response("Not found", { status: 404 })
     }
-  }
+  ).catch((err) => {
+    console.error(err)
+    return new Response("Internal server error", { status: 500 })
+  })
 }
