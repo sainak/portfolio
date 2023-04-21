28b4d337ae8efa7f99b80f0efab4b4bda2b0a287
Aakash Singh <mail@singhaakash.dev>
2023-04-21 15:44:49 +0530

    enable analytics

--- astro.config.ts
+++ astro.config.ts
@@ -7,5 +7,7 @@ import vercel from "@astrojs/vercel/serverless"
 export default defineConfig({
   output: "server",
   integrations: [preact(), tailwind()],
-  adapter: vercel(),
+  adapter: vercel({
+    analytics: true
+  }),
 })
