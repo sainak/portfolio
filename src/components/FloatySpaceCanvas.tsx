4b962f11bd27974ac9f4f6da1330e54452aea6bc
Aakash Singh <mail@singhaakash.dev>
2023-07-17 16:27:31 +0530

    fix wierd stroke color issue on chrome

--- src/components/FloatySpaceCanvas.tsx
+++ src/components/FloatySpaceCanvas.tsx
@@ -96,7 +96,7 @@ const FloatySpaceCanvas = ({ className = "" }) => {
           pt.size! -= 0.05
         }
 
-        form.stroke(`rgba(255,255,255,${pt.opacity}`).line([pt, lp])
+        form.strokeOnly(`rgba(255,255,255,${pt.opacity}`).line([pt, lp])
         // form.fillOnly(colors[i % 3]).point(pt, pt.size /* pt.size */, "circle")
         form
           .fillOnly(colors[i % 3])
